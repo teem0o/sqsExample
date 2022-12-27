6eb8302f07a9529858285ff19acebb94427c138d
temo <chxaidzetemo@gmail.com>
2022-12-27 22:30:01 +0400

    remove spring cloud, add aws

--- src/main/java/io/newAge/awsProject/sqs/controller/SqsController.java
+++ src/main/java/io/newAge/awsProject/sqs/controller/SqsController.java
@@ -1,26 +1,55 @@
 package io.newAge.awsProject.sqs.controller;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class SqsController {
-
-    @Autowired
-    QueueMessagingTemplate queueMessagingTemplate;
 
     @Value("${cloud.aws.end-point.uri}")
     private String queue;
 
+    @Autowired
+    private AmazonSQS amazonSQS;
 
     @GetMapping("/send/{message}")
-    public void sendMessage(@PathVariable String message) {
-        queueMessagingTemplate.send(queue, MessageBuilder.withPayload(message).build());
+    public String sendMessage(@PathVariable String message) {
+        Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
+        messageAttributes.put("AttributeOne", new MessageAttributeValue()
+                .withStringValue("This is an attribute")
+                .withDataType("String"));
+
+        SendMessageRequest sendMessageStandardQueue = new SendMessageRequest()
+                .withQueueUrl(queue)
+                .withMessageBody(message)
+//                .withDelaySeconds(5)
+                .withMessageAttributes(messageAttributes);
+
+        amazonSQS.sendMessage(sendMessageStandardQueue);
+        return "sent";
+    }
+
+    @GetMapping("/receive")
+    public List<Message> receiveMessage() {
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queue)
+                .withWaitTimeSeconds(10)
+                .withMaxNumberOfMessages(10);
+
+        List<Message> sqsMessages = amazonSQS.receiveMessage(receiveMessageRequest).getMessages();
+        return sqsMessages;
+    }
+    @GetMapping("/purge")
+    public PurgeQueueResult purgeQueue() {
+        return amazonSQS.purgeQueue(new PurgeQueueRequest(queue));
     }
 
 }
